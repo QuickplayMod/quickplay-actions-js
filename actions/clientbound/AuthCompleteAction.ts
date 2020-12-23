1a4cd777028d5ac631899701a7a11302c3d4400b
Erik <robere2@rpi.edu>
2020-12-23 11:28:14 -0500

    Added Hypixel account data to be sent to the client with AuthCompleteAction.ts

--- actions/clientbound/AuthCompleteAction.ts
+++ actions/clientbound/AuthCompleteAction.ts
@@ -15,6 +15,10 @@ import {Buffer} from 'buffer'
  * isAdmin boolean
  * isPremium boolean
  * Premium expiration timestamp, or 0 if user has no premium.
+ * Hypixel rank
+ * Hypixel package rank (combined packageRank,newPackageRank,monthlyPackageRank)
+ * Hypixel build team state
+ * Hypixel build team admin state
  */
 class AuthCompleteAction extends Action {
 
@@ -30,9 +34,14 @@ class AuthCompleteAction extends Action {
      * @param isAdmin {boolean} Whether this user has Admin permissions.
      * @param isPremium {boolean} Whether this user has a Premium subscription active.
      * @param premiumExpiration {Date} Datetime of when this user's Premium expires, or null if the user has no subscription.
+     * @param hypixelRank {string} Hypixel staff rank in the form of e.g. HELPER, ADMIN, YOUTUBER, etc.
+     * @param hypixelPackageRank Hypixel package rank in the form of e.g. NONE, VIP_PLUS, SUPERSTAR, etc.
+     * @param isHypixelBuildTeam Flag signifying whether the user is a Hypixel build team member or not.
+     * @param isHypixelBuildTeamAdmin Flag signifying whether the user is a Hypixel build team admin or not.
      */
     constructor (sessionToken?: string, sessionExpiration?: Date, mcUuid?: string, discordId?: string, googleId?: string,
-                 isAdmin?: boolean, isPremium?: boolean, premiumExpiration?: Date) {
+                 isAdmin?: boolean, isPremium?: boolean, premiumExpiration?: Date, hypixelRank?: string,
+                 hypixelPackageRank?: string, isHypixelBuildTeam?: boolean, isHypixelBuildTeamAdmin?: boolean) {
         super()
         this.id = 28
         if(sessionToken == undefined || sessionExpiration == undefined) {
@@ -55,6 +64,14 @@ class AuthCompleteAction extends Action {
         const premiumExpirationBuf = Buffer.alloc(4)
         premiumExpirationBuf.writeUInt32BE(premiumExpiration != null ? premiumExpiration.getTime() / 1000 : 0, 0)
         this.addPayload(premiumExpirationBuf)
+        this.addPayload(Buffer.from(hypixelRank || 'NONE'))
+        this.addPayload(Buffer.from(hypixelPackageRank || 'NONE'))
+        const isHypixelBuildTeamBuf = Buffer.alloc(1)
+        isHypixelBuildTeamBuf.writeUInt8(isHypixelBuildTeam ? 1 : 0, 0)
+        this.addPayload(isHypixelBuildTeamBuf)
+        const isHypixelBuildTeamAdminBuf = Buffer.alloc(1)
+        isHypixelBuildTeamAdminBuf.writeUInt8(isHypixelBuildTeamAdmin ? 1 : 0, 0)
+        this.addPayload(isHypixelBuildTeamAdminBuf)
     }
 }
 
